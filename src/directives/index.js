6f44da225ef09cba5bf5b2a9fb97eedda35c8f67
tiny-tinker <flashingtinker@gmail.com>
2018-09-03 23:50:24 +0800

    add utils for tooltip

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,8 @@
 import ClickOutside from './click-outside'
 import Tooltip from './tooltip'
-
-export {ClickOutside}
+import Popover from './popover'
+export {
+  ClickOutside,
+  Tooltip,
+  Popover
+}
